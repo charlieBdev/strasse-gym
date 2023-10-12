36c5585a1e6df2807b2f66c1b22a2b9e18d0a2da
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-10-12 12:51:33 +0100

    ready to add firestore

--- src/app/components/admin/NewsForm.jsx
+++ src/app/components/admin/NewsForm.jsx
@@ -1,24 +1,63 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { TinyMCEEditor } from '../admin/TinyMCEEditor';
 import { toast } from 'sonner';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../../../config';
+import Image from 'next/image';
+// import { TinyMCEEditor } from '../admin/TinyMCEEditor';
 
 export const NewsForm = () => {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
+	const [image, setImage] = useState(null);
+	const [imageUrl, setImageUrl] = useState(null);
+	const [uploading, setUploading] = useState(false);
+	const [uploadError, setUploadError] = useState(null);
+
+	const handleImageUpload = async (file) => {
+		// const file = e.target.files[0];
+
+		if (!file) return;
+
+		setUploadError(null);
+		setUploading(true);
+
+		const storageRef = ref(storage, 'images/' + file.name);
+
+		try {
+			const snapshot = await uploadBytes(storageRef, file);
+			const imageUrl = await getDownloadURL(snapshot.ref);
+
+			setImage(imageUrl);
+			setUploading(false);
+
+			console.log('Image URL:', imageUrl);
+		} catch (error) {
+			console.error('Error uploading image:', error);
+			setUploadError('Error uploading image. Please try again');
+			setUploading(false);
+		}
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (title && content) {
-			console.log(title, content);
+			if (image) {
+				handleImageUpload(image);
+			} else {
+				toast.error('Missing image');
+			}
+			console.log(title, content, image);
 		} else {
 			toast.error('Missing title or content');
 			console.log('Missing title or content');
 		}
 	};
 
-	const handleImageUpload = () => {
-		console.log('Image uploading kinds');
+	const handleFileChange = (e) => {
+		const file = e.target.files[0];
+		handleImageUpload(file);
+		setImageUrl(URL.createObjectURL(file));
 	};
 
 	return (
@@ -35,7 +74,7 @@ export const NewsForm = () => {
 					value={title}
 					onChange={(e) => setTitle(e.target.value)}
 				/>
-
+				{/* <TinyMCEEditor content={content} setContent={setContent} /> */}
 				<textarea
 					rows="9"
 					type="text"
@@ -44,14 +83,18 @@ export const NewsForm = () => {
 					value={content}
 					onChange={(e) => setContent(e.target.value)}
 				/>
-
-				<TinyMCEEditor content={content} setContent={setContent} />
 				<input
 					type="file"
 					accept="image/*"
-					onChange={handleImageUpload}
+					onChange={handleFileChange}
 					className="rounded-lg"
 				/>
+				{uploading && <p>Oooooo Weeeee</p>}
+				{uploadError && <p className="text-red-800">{uploadError}</p>}
+				{image && (
+					<Image src={imageUrl} alt="To upload" width={150} height={150} />
+				)}
+
 				<motion.button
 					className="bg-neutral-50 text-neutral-950 font-medium rounded-full px-3 py-1 mx-auto font-headings"
 					whileHover={{ scale: 1.1 }}
