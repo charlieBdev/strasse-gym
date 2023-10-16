e06a4c4d0870c63d48e783c50dd646a8b0ba88ad
charlieBdev <chazbish@hotmail.com>
2023-10-16 18:45:04 +0100

    tidy up news form

--- src/app/components/admin/NewsForm.jsx
+++ src/app/components/admin/NewsForm.jsx
@@ -10,6 +10,7 @@ import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
 export const NewsForm = () => {
 	const fileInputRef = useRef(null);
+
 	const [isUploadingFile, setIsUploadingFile] = useState(false);
 	const [fileUploaded, setFileUploaded] = useState(false);
 	const [isSubmittingForm, setIsSubmittingForm] = useState(false);
@@ -19,9 +20,10 @@ export const NewsForm = () => {
 	const initialData = {
 		title: '',
 		content: '',
+		imageAlt: '',
 	};
 	const [data, setData] = useState(initialData);
-	const { title, content } = data;
+	const { title, content, imageAlt } = data;
 
 	const [file, setFile] = useState(null);
 
@@ -36,55 +38,56 @@ export const NewsForm = () => {
 		}
 		setFile(null);
 		setFileUploaded(false);
+		setErrors({});
 	};
 
-	useEffect(() => {
-		const uploadFile = () => {
-			const name = new Date().getTime() + `_${file.name}`;
-			setIsUploadingFile(true);
-			setFileUploaded(false);
-			const storageRef = ref(storage, '/news/' + name);
-			const uploadTask = uploadBytesResumable(storageRef, file);
+	// useEffect(() => {
+	// 	const uploadFile = () => {
+	// 		const name = new Date().getTime() + `_${file.name}`;
+	// 		setIsUploadingFile(true);
+	// 		setFileUploaded(false);
+	// 		const storageRef = ref(storage, '/news/' + name);
+	// 		const uploadTask = uploadBytesResumable(storageRef, file);
 
-			uploadTask.on(
-				'state_changed',
-				(snapshot) => {
-					const progress =
-						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-					setProgress(progress);
-					switch (snapshot.state) {
-						case 'paused':
-							console.log('Upload is paused');
-							break;
-						case 'running':
-							console.log('Upload is running');
-							break;
-						default:
-							break;
-					}
-				},
-				(error) => {
-					console.log(error);
-					setFileUploaded(false);
-				},
-				() => {
-					getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-						// console.log(downloadURL, '<<< downloadURL');
-						setData((prev) => {
-							return { ...prev, imageUrl: downloadURL };
-						});
-						toast.success('Image uploaded!');
-						setFileUploaded(true);
-						setIsUploadingFile(false);
-					});
-				}
-			);
-		};
-		// file && uploadFile();
-		if (file) {
-			uploadFile();
-		}
-	}, [file]);
+	// 		uploadTask.on(
+	// 			'state_changed',
+	// 			(snapshot) => {
+	// 				const progress =
+	// 					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+	// 				setProgress(progress);
+	// 				switch (snapshot.state) {
+	// 					case 'paused':
+	// 						console.log('Upload is paused');
+	// 						break;
+	// 					case 'running':
+	// 						console.log('Upload is running');
+	// 						break;
+	// 					default:
+	// 						break;
+	// 				}
+	// 			},
+	// 			(error) => {
+	// 				console.log(error);
+	// 				setFileUploaded(false);
+	// 			},
+	// 			() => {
+	// 				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+	// 					// console.log(downloadURL, '<<< downloadURL');
+	// 					setData((prev) => {
+	// 						return { ...prev, imageUrl: downloadURL };
+	// 					});
+	// 					toast.success('Image uploaded!');
+	// 					setFileUploaded(true);
+	// 					setIsUploadingFile(false);
+	// 				});
+	// 			}
+	// 		);
+	// 	};
+	// 	// file && uploadFile();
+	// 	if (file) {
+	// 		uploadFile();
+	// 	}
+	// }, [file]);
 
 	const validate = () => {
 		let errors = {};
@@ -97,33 +100,92 @@ export const NewsForm = () => {
 		if (!file) {
 			errors.file = 'Please choose an image file';
 		}
+		if (!imageAlt) {
+			errors.imageAlt = 'Image alt is required';
+		}
 		return errors;
 	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		setFileUploaded(false);
-		setIsSubmittingForm(true);
-		setErrors({});
+
+		if (!file) {
+			return setErrors({ file: 'Please choose an image file' });
+		}
 
 		let errors = validate();
+
 		if (Object.keys(errors).length) {
-			setIsSubmittingForm(false);
 			return setErrors(errors);
 		}
 
-		try {
-			await addDoc(collection(db, 'news'), {
-				...data,
-				created: serverTimestamp(),
-			});
-			toast.success('News added!');
-			clearForm();
-		} catch (error) {
-			toast.error('Error adding news');
-		}
-		// navigate('/')
-		setIsSubmittingForm(false);
+		// new
+
+		setIsUploadingFile(true);
+
+		const name = new Date().getTime() + `_${file.name}`;
+		const storageRef = ref(storage, '/news/' + name);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on(
+			'state_changed',
+			(snapshot) => {
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				setProgress(progress);
+				// switch (snapshot.state) {
+				// 	case 'paused':
+				// 		console.log('Upload is paused');
+				// 		break;
+				// 	case 'running':
+				// 		console.log('Upload is running');
+				// 		break;
+				// 	default:
+				// 		break;
+				// }
+			},
+			// unnsuccessful image uploads
+			(error) => {
+				console.log(error);
+				setIsUploadingFile(false);
+				return toast.error('Error uploading image file');
+			},
+			// successful image uploads
+			async () => {
+				setIsUploadingFile(false);
+				setFileUploaded(true);
+				setIsSubmittingForm(true);
+
+				try {
+					const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+					const newsData = {
+						...data,
+						imageUrl: downloadURL,
+						created: serverTimestamp(),
+					};
+					await addDoc(collection(db, 'news'), newsData);
+					toast.success('News added!');
+					clearForm();
+				} catch (error) {
+					return toast.error('Error adding news (Firestore)');
+				} finally {
+					setIsSubmittingForm(false);
+				}
+			}
+		);
+
+		// try {
+		// 	await addDoc(collection(db, 'news'), {
+		// 		...data,
+		// 		created: serverTimestamp(),
+		// 	});
+		// 	toast.success('News added!');
+		// 	clearForm();
+		// } catch (error) {
+		// 	toast.error('Error adding news');
+		// }
+		// // navigate('/')
+		// setIsSubmittingForm(false);
 	};
 
 	return (
@@ -140,7 +202,7 @@ export const NewsForm = () => {
 					type="text"
 					placeholder="Add a title"
 					className={`${
-						title.length ? 'bg-green-500' : ''
+						title.length ? ' bg-green-500' : ''
 					} rounded-lg px-2 py-1 text-neutral-950 w-full`}
 					value={title}
 					name="title"
@@ -167,13 +229,28 @@ export const NewsForm = () => {
 				<input
 					type="file"
 					accept="image/*"
-					className={`${fileUploaded ? 'bg-green-500' : ''} rounded-lg`}
+					className={`${
+						file ? 'bg-green-500 text-neutral-950' : ''
+					} rounded-lg`}
 					onChange={(e) => setFile(e.target.files[0])}
 					disabled={isUploadingFile || isSubmittingForm}
 					ref={fileInputRef}
 				/>
 				{errors.file && !file && <p className="text-red-500">{errors.file}</p>}
 				{isUploadingFile && <p>Uploading image... {Math.floor(progress)}%</p>}
+				<input
+					type="text"
+					placeholder="Add an image description"
+					className={`${
+						imageAlt.length ? 'bg-green-500' : ''
+					} rounded-lg px-2 py-1 text-neutral-950 w-full`}
+					value={imageAlt}
+					name="imageAlt"
+					onChange={handleChange}
+				/>
+				{errors.imageAlt && imageAlt.length === 0 && (
+					<p className="text-red-500">{errors.imageAlt}</p>
+				)}
 				<motion.button
 					className="bg-neutral-50 text-neutral-950 font-medium rounded-full px-3 py-1 mx-auto font-headings"
 					whileHover={{ scale: 1.1 }}
