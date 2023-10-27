4065d9d390afe57b81a913f146f39838694d0864
charlieBdev <chazbish@hotmail.com>
2023-10-27 15:51:10 +0100

    changed hero

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 export const Button = ({ text }) => {
 	return (
 		<motion.button
-			className="bg-neutral-50 hover:bg-neutral-900 text-neutral-900 hover:text-neutral-50 hover:ring-2 hover:ring-neutral-50 ring-inset font-medium rounded-full px-3 py-1 mx-auto"
+			className='bg-neutral-50 hover:bg-neutral-900 text-neutral-900 hover:text-neutral-50 hover:ring-2 hover:ring-neutral-50 ring-inset font-medium rounded-full px-3 py-1 mx-auto text-md md:text-lg lg:text-xl xl:text-2xl'
 			// whileHover={{ scale: 1.1 }}
 			whileTap={{ scale: 0.9 }}
 		>
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,42 +6,55 @@ import { UpDown } from './UpDown';
 // import hero from '/hero.jpg';
 
 export const Hero = () => {
-	const mainText = 'Stand up and fight!';
+	const backgroundImageUrl = '/hero.jpg';
+	const mainText = 'Unleash Your Inner Warrior!';
 	const classes = ['Kickboxing', 'BJJ', 'KAPAP'];
 
 	return (
 		<section
 			id='hero'
 			className='h-[calc(100%-5rem)] w-full flex flex-col items-center justify-between gap-3 px-6 md:px-16 lg:px-24 xl:px-32'
+			style={{
+				backgroundImage: `url(${backgroundImageUrl})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+			}}
 		>
 			<div></div>
 			<div className='flex flex-col md:flex-row items-center justify-center w-full gap-3'>
-				<div className='flex flex-col gap-6 w-full select-none'>
-					<h2 className='text-lg italic font-semibold uppercase'>{mainText}</h2>
+				<div className='flex flex-col gap-6 w-full select-none bg-neutral-950 opacity-90 p-3 rounded-lg'>
+					<h2 className='text-xl md:text-2xl lg:text-3xl xl:text-4xl italic uppercase'>
+						{mainText}
+					</h2>
 					<ul className='list-disc list-inside'>
 						{classes.map((classname) => (
-							<li key={classname} className='text-lg'>
+							<li
+								key={classname}
+								className='text-lg md:text-xl lg:text-2xl xl:text-3xl'
+							>
 								{classname}
 							</li>
 						))}
 					</ul>
-					<h3 className='text-sm'>@ Meersbrooke, Sheffield</h3>
+					<h3 className='text-md md:text-lg lg:text-xl xl:text-2xl'>
+						Meersbrook, Sheffield
+					</h3>
 					<Link href='#contact' className='mx-auto'>
 						<Button text='Get in touch' />
 					</Link>
 				</div>
-				<Image
+				{/* <Image
 					style={{ objectFit: 'cover' }}
 					// fill={true}
 					// placeholder="blur"
 					priority
 					// loading="lazy"
-					src='/hero.jpg'
+					src={backgroundImageUrl}
 					alt="Two men competing in a BJJ competition on the floor. One, on the bottom, looks like but isn't Craig."
 					width={750}
 					height={750}
 					className='rounded-sm h-full w-full md:w-1/2 aspect-[16/9] shadow-xl shadow-neutral-950'
-				/>
+				/> */}
 			</div>
 			<UpDown href={'news'} direction={'down'} bounce={true} />
 		</section>
