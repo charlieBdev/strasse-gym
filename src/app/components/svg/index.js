d8bd525c69d83ea0f893584087ad12f4f7f0e221
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-12-13 09:37:25 +0000

    add index file to SVG folder

---
+++ src/app/components/svg/index.js
@@ -0,0 +1,47 @@
+import { AddNewsSVG } from './AddNewsSVG';
+import { AdminSVG } from './AdminSVG';
+import { BackSVG } from './BackSVG';
+import { CalendarSVG } from './CalendarSVG';
+import { CallSVG } from './CallSVG';
+import { CancelDeleteSVG } from './CancelDeleteSVG';
+import { ClockSVG } from './ClockSVG';
+import { ConfirmDeleteSVG } from './ConfirmDeleteSVG';
+import { DaytimeSVG } from './DaytimeSVG';
+import { DeleteSVG } from './DeleteSVG';
+import { EditSVG } from './EditSVG';
+import { EmailSVG } from './EmailSVG';
+import { EveningSVG } from './EveningSVG';
+import { HomeSVG } from './HomeSVG';
+import { ImageSVG } from './ImageSVG';
+import { NewsSVG } from './NewsSVG';
+import { NextSVG } from './NextSVG';
+import { PinSVG } from './PinSVG';
+import { PreviousSVG } from './PreviousSVG';
+import { SignoutSVG } from './SignoutSVG';
+import { SwapImageSVG } from './SwapImageSVG';
+import { WarningSVG } from './WarningSVG';
+
+export {
+	AddNewsSVG,
+	AdminSVG,
+	BackSVG,
+	CalendarSVG,
+	CallSVG,
+	CancelDeleteSVG,
+	ClockSVG,
+	ConfirmDeleteSVG,
+	DaytimeSVG,
+	DeleteSVG,
+	EditSVG,
+	EmailSVG,
+	EveningSVG,
+	HomeSVG,
+	ImageSVG,
+	NewsSVG,
+	NextSVG,
+	PinSVG,
+	PreviousSVG,
+	SignoutSVG,
+	SwapImageSVG,
+	WarningSVG,
+};
