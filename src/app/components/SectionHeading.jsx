0c1204727faa32b37201646d0c2f606edc139dbf
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-12-13 11:48:57 +0000

    change fadeIn direction

--- src/app/components/SectionHeading.jsx
+++ src/app/components/SectionHeading.jsx
@@ -5,7 +5,7 @@ export const SectionHeading = ({ heading }) => {
 	return (
 		<motion.p
 			className='mx-auto text-center italic font-semibold text-md bg-gradient-to-r from-blue-500 to-purple-500 border-l border-b-2 border-neutral-50 p-3 select-none rounded-sm -rotate-3 hover:rotate-0'
-			variants={fadeIn('left', 'spring', 0.2, 1)}
+			variants={fadeIn('right', 'spring', 0.2, 1)}
 		>
 			{heading}
 		</motion.p>
